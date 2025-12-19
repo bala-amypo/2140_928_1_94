bc7890619ec79ce6df3a92c16d7c4f11b587d5e0
bala-amypo <bala@amypo.in>
2025-12-19 14:16:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/DateTimeUtil.java
@@ -0,0 +1,10 @@
+package com.example.demo.util;
+
+import java.time.LocalDateTime;
+
+public class DateTimeUtil {
+
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+}
