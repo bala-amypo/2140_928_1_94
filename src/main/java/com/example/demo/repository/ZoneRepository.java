755463ceaa0897d317afcf5dd7565340c4ea77ae
bala-amypo <bala@amypo.in>
2025-12-20 12:15:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -3,8 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
-    Optional<Zone> findByZoneName(String zoneName);
 }
--- src/main/java/com/example/demo/service/BinService.java
+++ src/main/java/com/example/demo/service/BinService.java
@@ -4,14 +4,9 @@ import com.example.demo.model.Bin;
 import java.util.List;
 
 public interface BinService {
-
     Bin createBin(Bin bin);
-
     Bin getBinById(Long id);
-
     List<Bin> getAllBins();
-
     Bin updateBin(Long id, Bin bin);
-
     void deactivateBin(Long id);
 }
